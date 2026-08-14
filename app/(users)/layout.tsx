4a5460acf2b0f739da0156fa5adac25ef23b79c8
MokaniJay <jayj79967@gmail.com>
2026-08-14 15:07:39 +0530

    Updated project changes

--- app/(users)/layout.tsx
+++ app/(users)/layout.tsx
@@ -22,13 +22,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    alert(
-   "SkillCircle is currently presented as an academic prototype.\n" +
-"While core features demonstrate the concept, full-scale functionality and business deployment are under ongoing development.\n" +
-"This project is intended to evolve into a complete real-world platform."
-    );
-  }, []);
+
 
   return (
     <html lang="en">
